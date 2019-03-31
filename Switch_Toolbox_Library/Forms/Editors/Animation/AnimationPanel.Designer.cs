decd83c487a948c3b47d6b609a7594da8abddab9
KillzXGaming <millernathan35@gmail.com>
2019-03-31 18:23:41 -0400

    More timeline fixes

---
+++ Switch_Toolbox_Library/Forms/Editors/Animation/AnimationPanel.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Switch_Toolbox.Library
+{
+    partial class AnimationPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.loopChkBox = new Switch_Toolbox.Library.Forms.STCheckBox();
+            this.totalFrame = new System.Windows.Forms.NumericUpDown();
+            this.currentFrameUpDown = new System.Windows.Forms.NumericUpDown();
+            this.animationPlayBtn = new System.Windows.Forms.Button();
+            this.stPanel1 = new Switch_Toolbox.Library.Forms.STPanel();
+            this.animationTrackBar = new Switch_Toolbox.Library.Forms.TimeLine();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.totalFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currentFrameUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.BackgroundImage = global::Switch_Toolbox.Library.Properties.Resources.arrowL;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(17, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 21);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.panel1.Controls.Add(this.loopChkBox);
+            this.panel1.Controls.Add(this.totalFrame);
+            this.panel1.Controls.Add(this.currentFrameUpDown);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.animationPlayBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 162);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(631, 32);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // loopChkBox
+            // 
+            this.loopChkBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.loopChkBox.AutoSize = true;
+            this.loopChkBox.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.loopChkBox.Checked = true;
+            this.loopChkBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.loopChkBox.Location = new System.Drawing.Point(318, 10);
+            this.loopChkBox.Name = "loopChkBox";
+            this.loopChkBox.Size = new System.Drawing.Size(50, 17);
+            this.loopChkBox.TabIndex = 5;
+            this.loopChkBox.Text = "Loop";
+            this.loopChkBox.UseVisualStyleBackColor = true;
+            // 
+            // totalFrame
+            // 
+            this.totalFrame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalFrame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.totalFrame.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.totalFrame.ForeColor = System.Drawing.Color.White;
+            this.totalFrame.Location = new System.Drawing.Point(508, 12);
+            this.totalFrame.Name = "totalFrame";
+            this.totalFrame.Size = new System.Drawing.Size(98, 16);
+            this.totalFrame.TabIndex = 4;
+            // 
+            // currentFrameUpDown
+            // 
+            this.currentFrameUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.currentFrameUpDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.currentFrameUpDown.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.currentFrameUpDown.ForeColor = System.Drawing.Color.White;
+            this.currentFrameUpDown.Location = new System.Drawing.Point(390, 12);
+            this.currentFrameUpDown.Name = "currentFrameUpDown";
+            this.currentFrameUpDown.Size = new System.Drawing.Size(98, 16);
+            this.currentFrameUpDown.TabIndex = 3;
+            this.currentFrameUpDown.ValueChanged += new System.EventHandler(this.currentFrameUpDown_ValueChanged);
+            // 
+            // animationPlayBtn
+            // 
+            this.animationPlayBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.animationPlayBtn.BackgroundImage = global::Switch_Toolbox.Library.Properties.Resources.arrowR;
+            this.animationPlayBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.animationPlayBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.animationPlayBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.animationPlayBtn.Location = new System.Drawing.Point(57, 7);
+            this.animationPlayBtn.Name = "animationPlayBtn";
+            this.animationPlayBtn.Size = new System.Drawing.Size(31, 21);
+            this.animationPlayBtn.TabIndex = 1;
+            this.animationPlayBtn.UseVisualStyleBackColor = false;
+            this.animationPlayBtn.Click += new System.EventHandler(this.animationPlayBtn_Click);
+            // 
+            // stPanel1
+            // 
+            this.stPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stPanel1.Location = new System.Drawing.Point(0, 0);
+            this.stPanel1.Name = "stPanel1";
+            this.stPanel1.Size = new System.Drawing.Size(631, 16);
+            this.stPanel1.TabIndex = 5;
+            // 
+            // animationTrackBar
+            // 
+            this.animationTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.animationTrackBar.CurrentFrame = 0;
+            this.animationTrackBar.FrameCount = 1000;
+            this.animationTrackBar.Location = new System.Drawing.Point(0, 14);
+            this.animationTrackBar.Name = "animationTrackBar";
+            this.animationTrackBar.Size = new System.Drawing.Size(631, 149);
+            this.animationTrackBar.TabIndex = 6;
+            // 
+            // AnimationPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.animationTrackBar);
+            this.Controls.Add(this.stPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "AnimationPanel";
+            this.Size = new System.Drawing.Size(631, 194);
+            this.Load += new System.EventHandler(this.AnimationPanel_Load);
+            this.Click += new System.EventHandler(this.AnimationPanel_Click);
+            this.Enter += new System.EventHandler(this.AnimationPanel_Enter);
+            this.Leave += new System.EventHandler(this.AnimationPanel_Leave);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.totalFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currentFrameUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button animationPlayBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown totalFrame;
+        private System.Windows.Forms.NumericUpDown currentFrameUpDown;
+        private Forms.STPanel stPanel1;
+        private Forms.STCheckBox loopChkBox;
+        private Forms.TimeLine animationTrackBar;
+    }
+}
---
+++ Switch_Toolbox_Library/Forms/Editors/Animation/TimeLine.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_Toolbox.Library.Forms
+{
+    public partial class TimeLine : UserControl
+    {
+        public TimeLine()
+        {
+            InitializeComponent();
+            timer.Interval = 10;
+            timer.Tick += Timer_Tick;
+        }
+
+        public void Play() { }
+        public void Stop() { }
+
+        protected int margin = 0;
+
+        public event EventHandler FrameChanged;
+
+        private Timer timer = new Timer();
+
+        public bool Locked { get; private set; } = false;
+
+        private void ResolveCollision()
+        {
+            if (frameLeft < 0)
+            {
+                frameRight -= frameLeft;
+                frameLeft = 0;
+            }
+            else if (frameRight > lastFrame)
+            {
+                frameLeft += lastFrame - frameRight;
+                frameRight = lastFrame;
+            }
+        }
+
+        private void ResolveFitting()
+        {
+            if (frameLeft < 0)
+            {
+                frameRight -= frameLeft;
+                if (frameRight > lastFrame)
+                    frameRight = lastFrame;
+                frameLeft = 0;
+            }
+            else if (frameRight > lastFrame)
+            {
+                frameLeft += lastFrame - frameRight;
+                if (frameLeft < 0)
+                    frameLeft = 0;
+                frameRight = lastFrame;
+            }
+        }
+
+        public int CurrentFrame
+        {
+            get => currentFrame;
+            set
+            {
+                if (FollowCurrentFrame && !(Focused && MouseButtons == MouseButtons.Right))
+                {
+                    double delta = value - (frameRight + frameLeft) * 0.5;
+                    frameLeft += delta;
+                    frameRight += delta;
+
+                    ResolveCollision();
+
+                }
+
+                currentFrame = value;
+
+                Refresh();
+            }
+        }
+
+        public int FrameCount
+        {
+            get => lastFrame + 1;
+            set
+            {
+                lastFrame = value - 1;
+
+                if (value == 1)
+                {
+                    frameLeft = 0;
+                    frameRight = 1;
+                }
+                else
+                {
+                    ResolveFitting();
+                }
+
+                if (currentFrame > lastFrame)
+                    currentFrame = lastFrame;
+
+                Refresh();
+            }
+        }
+
+        public bool FollowCurrentFrame = true;
+
+        protected int currentFrame = 0;
+        protected int lastFrame = 1000;
+
+        protected double frameLeft = 0;
+        protected double frameRight = 200;
+
+        protected Point lastMousePos;
+
+
+        protected static Brush brush1 = new SolidBrush(Color.FromArgb(255, 255, 20));
+        protected static Brush brush2 = new SolidBrush(Color.FromArgb(20, 20, 20));
+        protected static Brush brush3 = new SolidBrush(Color.FromArgb(50, 50, 50));
+        protected static Brush brush4 = new SolidBrush(Color.FromArgb(90, 90, 90));
+        protected static Brush brush5 = new SolidBrush(Color.FromArgb(150, 150, 150));
+
+        protected static Pen pen1 = new Pen(new SolidBrush(Color.FromArgb(30, 30, 30)), 2);
+        protected static Pen pen2 = new Pen(new SolidBrush(Color.FromArgb(100, 100, 20)), 2);
+        protected static Pen pen3 = new Pen(new SolidBrush(Color.FromArgb(150, 150, 150)), 2);
+
+        protected static Font font = new Font(new FontFamily("arial"), 10);
+
+        protected static int barHeight = TextRenderer.MeasureText("0", font).Height;
+
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.SetClip(new Rectangle(margin, 0, Width - margin, Height));
+
+            double currentFrameX = 20 + margin + (currentFrame - frameLeft) * (Width - 40 - margin) / (frameRight - frameLeft);
+
+            e.Graphics.FillRectangle(brush2, new Rectangle(0, 0, Width, Height));
+            e.Graphics.FillRectangle(brush3, new Rectangle(0, 0, Width, barHeight));
+
+            double step = 50 * (frameRight - frameLeft) / Width;
+            if (step > 10)
+                step = Math.Round(step / 10.0) * 10;
+            else
+            {
+                step = Math.Round(Math.Max(1, step));
+            }
+
+            if (lastFrame != 0)
+            {
+                double max;
+                if (frameRight < lastFrame)
+                    max = Math.Min(frameRight + step, lastFrame);
+                else
+                    max = frameRight - step;
+
+                for (double frame = Math.Floor(frameLeft / step) * step; frame <= max; frame += step)
+                {
+                    double frameX = 20 + margin + (frame - frameLeft) * (Width - 40 - margin) / (frameRight - frameLeft);
+
+                    e.Graphics.DrawLine(pen1, new Point((int)frameX, barHeight), new Point((int)frameX, Height));
+
+                    e.Graphics.DrawString("" + frame, font, brush4, new Point((int)frameX - TextRenderer.MeasureText("" + frame, font).Width / 2, 0));
+                }
+            }
+
+            if (frameRight == lastFrame)
+            {
+                //draw last frame regardless of the steps
+                double x = Width - 20;
+
+                e.Graphics.DrawLine(pen1, new Point((int)x, barHeight), new Point((int)x, Height));
+
+                e.Graphics.DrawString("" + lastFrame, font, brush4, new Point((int)x - TextRenderer.MeasureText("" + lastFrame, font).Width / 2, 0));
+            }
+            /*
+			int lastY = value((int)(Math.Round(- 20+margin * (frameRight - frameLeft) / (Width - 40-margin) + frameLeft) + lastFrame + 1) % (lastFrame + 1));
+			for (int x = 5; x < Width+10; x+=5)
+			{
+				e.Graphics.DrawLine(pen3, x - 5, lastY, x, lastY =
+					value((int)(Math.Round((x - 20+margin) * (frameRight - frameLeft) / (Width - 40-margin) + frameLeft) + lastFrame + 1) % (lastFrame + 1))
+					);
+			}
+			*/
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.SetClip(new Rectangle(margin, 0, Width - margin, Height));
+
+            double currentFrameX = 20 + margin + (currentFrame - frameLeft) * (Width - 40 - margin) / (frameRight - frameLeft);
+            base.OnPaint(e);
+            e.Graphics.DrawLine(pen2, new Point((int)currentFrameX, barHeight), new Point((int)currentFrameX, Height));
+
+            e.Graphics.DrawString("" + currentFrame, font, brush1, new Point((int)currentFrameX - TextRenderer.MeasureText("" + currentFrame, font).Width / 2, 0));
+        }
+
+        private int value(int frame)
+        {
+            return frame + 30;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            double step;
+            if (lastMousePos.X < 20 + margin)
+            {
+                step = (20 + margin - lastMousePos.X) * (frameRight - frameLeft) / Width;
+                frameLeft -= step;
+                frameRight -= step;
+
+                #region resolve collsions
+                if (frameLeft < 0)
+                {
+                    frameRight -= frameLeft;
+                    frameLeft = 0;
+                }
+                #endregion
+            }
+            else
+            {
+                step = (lastMousePos.X - Width + 20) * (frameRight - frameLeft) / Width;
+                frameLeft += step;
+                frameRight += step;
+
+                #region resolve collsions
+                if (frameRight > lastFrame)
+                {
+                    frameLeft += lastFrame - frameRight;
+                    frameRight = lastFrame;
+                }
+                #endregion
+            }
+
+            currentFrame = Math.Min(Math.Max(0, (int)Math.Round(((lastMousePos.X - 20 - margin) * (frameRight - frameLeft) / (Width - 40 - margin) + frameLeft))), lastFrame);
+            FrameChanged?.Invoke(this, new EventArgs());
+            Refresh();
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (lastFrame == 0)
+                return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+
+                timer.Enabled = (e.X < 20 + margin || e.X > Width - 20);
+                Locked = true;
+                currentFrame = Math.Min(Math.Max(0, (int)Math.Round(((e.X - 20 - margin) * (frameRight - frameLeft) / (Width - 40 - margin) + frameLeft))), lastFrame);
+                FrameChanged?.Invoke(this, new EventArgs());
+                Refresh();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                double delta = (e.X - lastMousePos.X) * (frameRight - frameLeft) / (Width - 40 - margin);
+                frameLeft -= delta;
+                frameRight -= delta;
+
+                ResolveCollision();
+
+                Refresh();
+            }
+
+            lastMousePos = e.Location;
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            timer.Stop();
+            Locked = false;
+            base.OnMouseUp(e);
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            if (lastFrame == 0)
+                return;
+
+            if (frameRight - frameLeft <= 2 && e.Delta > 0)
+                return;
+
+            double delta = 1 + Math.Min(Math.Max(-0.5, -e.Delta * 0.00390625), 0.5);
+
+            double frameOrigin = Math.Min(Math.Max(0, ((e.X - 20 - margin) * (frameRight - frameLeft) / (Width - 40 - margin) + frameLeft)), lastFrame);
+
+            frameLeft = Math.Min(-1, (frameLeft - frameOrigin)) * delta + frameOrigin;
+            frameRight = Math.Max(1, (frameRight - frameOrigin)) * delta + frameOrigin;
+
+            ResolveFitting();
+
+            Refresh();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            Refresh();
+            base.OnResize(e);
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
+    }
+}
