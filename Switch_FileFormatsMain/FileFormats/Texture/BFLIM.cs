7eaf59805df3dc04c0f46af3be6b137e8f75bad9
KillzXGaming <millernathan35@gmail.com>
2019-04-18 21:41:39 -0400

    Fix bflim args

--- Switch_FileFormatsMain/FileFormats/Texture/BFLIM.cs
+++ Switch_FileFormatsMain/FileFormats/Texture/BFLIM.cs
@@ -150,7 +150,7 @@ namespace FirstPlugin
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 FTEX ftex = new FTEX();
-                ftex.ReplaceTexture(ofd.FileName, 1, SupportedFormats);
+                ftex.ReplaceTexture(ofd.FileName, 1, SupportedFormats, true, true);
                 if (ftex.texture != null)
                 {
                     image.Swizzle = (byte)ftex.texture.Swizzle;
