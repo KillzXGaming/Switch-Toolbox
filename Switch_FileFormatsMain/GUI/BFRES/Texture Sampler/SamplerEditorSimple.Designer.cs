ef689c290a9ed9ba88f7ce2c73ba56cbebe3989d
KillzXGaming <millernathan35@gmail.com>
2019-04-23 15:27:20 -0400

    Update UI a bit

--- Switch_FileFormatsMain/GUI/BFRES/Texture Sampler/SamplerEditorSimple.Designer.cs
+++ Switch_FileFormatsMain/GUI/BFRES/Texture Sampler/SamplerEditorSimple.Designer.cs
@@ -29,22 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SamplerEditorSimple));
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.stPanel1 = new Switch_Toolbox.Library.Forms.STPanel();
-            this.stLabel3 = new Switch_Toolbox.Library.Forms.STLabel();
-            this.stTextBox2 = new Switch_Toolbox.Library.Forms.STTextBox();
-            this.samplerCB = new Switch_Toolbox.Library.Forms.STComboBox();
-            this.stLabel2 = new Switch_Toolbox.Library.Forms.STLabel();
-            this.stButton1 = new Switch_Toolbox.Library.Forms.STButton();
-            this.nameTB = new Switch_Toolbox.Library.Forms.STTextBox();
-            this.stLabel1 = new Switch_Toolbox.Library.Forms.STLabel();
             this.stPropertyGrid1 = new Switch_Toolbox.Library.Forms.STPropertyGrid();
+            this.stLabel1 = new Switch_Toolbox.Library.Forms.STLabel();
+            this.nameTB = new Switch_Toolbox.Library.Forms.STTextBox();
+            this.stButton1 = new Switch_Toolbox.Library.Forms.STButton();
+            this.stLabel2 = new Switch_Toolbox.Library.Forms.STLabel();
+            this.samplerCB = new Switch_Toolbox.Library.Forms.STComboBox();
+            this.stTextBox2 = new Switch_Toolbox.Library.Forms.STTextBox();
+            this.stLabel3 = new Switch_Toolbox.Library.Forms.STLabel();
             this.textureBP = new Switch_Toolbox.Library.Forms.PictureBoxCustom();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
@@ -52,24 +47,6 @@
             this.stPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.textureBP)).BeginInit();
             this.SuspendLayout();
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.textureBP);
-            this.splitContainer1.Size = new System.Drawing.Size(585, 522);
-            this.splitContainer1.SplitterDistance = 299;
-            this.splitContainer1.TabIndex = 0;
             // 
             // splitContainer2
             // 
@@ -84,12 +61,13 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.stPropertyGrid1);
-            this.splitContainer2.Size = new System.Drawing.Size(585, 299);
+            this.splitContainer2.Size = new System.Drawing.Size(585, 432);
             this.splitContainer2.SplitterDistance = 277;
             this.splitContainer2.TabIndex = 0;
             // 
             // stPanel1
             // 
+            this.stPanel1.Controls.Add(this.textureBP);
             this.stPanel1.Controls.Add(this.stLabel3);
             this.stPanel1.Controls.Add(this.stTextBox2);
             this.stPanel1.Controls.Add(this.samplerCB);
@@ -100,25 +78,54 @@
             this.stPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.stPanel1.Location = new System.Drawing.Point(0, 0);
             this.stPanel1.Name = "stPanel1";
-            this.stPanel1.Size = new System.Drawing.Size(277, 299);
+            this.stPanel1.Size = new System.Drawing.Size(277, 432);
             this.stPanel1.TabIndex = 0;
             // 
-            // stLabel3
+            // stPropertyGrid1
             // 
-            this.stLabel3.AutoSize = true;
-            this.stLabel3.Location = new System.Drawing.Point(5, 87);
-            this.stLabel3.Name = "stLabel3";
-            this.stLabel3.Size = new System.Drawing.Size(29, 13);
-            this.stLabel3.TabIndex = 6;
-            this.stLabel3.Text = "Hint:";
+            this.stPropertyGrid1.AutoScroll = true;
+            this.stPropertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.stPropertyGrid1.Location = new System.Drawing.Point(0, 0);
+            this.stPropertyGrid1.Name = "stPropertyGrid1";
+            this.stPropertyGrid1.ShowHintDisplay = true;
+            this.stPropertyGrid1.Size = new System.Drawing.Size(304, 432);
+            this.stPropertyGrid1.TabIndex = 0;
             // 
-            // stTextBox2
+            // stLabel1
             // 
-            this.stTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.stTextBox2.Location = new System.Drawing.Point(67, 85);
-            this.stTextBox2.Name = "stTextBox2";
-            this.stTextBox2.Size = new System.Drawing.Size(207, 20);
-            this.stTextBox2.TabIndex = 5;
+            this.stLabel1.AutoSize = true;
+            this.stLabel1.Location = new System.Drawing.Point(5, 10);
+            this.stLabel1.Name = "stLabel1";
+            this.stLabel1.Size = new System.Drawing.Size(46, 13);
+            this.stLabel1.TabIndex = 0;
+            this.stLabel1.Text = "Texture:";
+            // 
+            // nameTB
+            // 
+            this.nameTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nameTB.Location = new System.Drawing.Point(8, 32);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(207, 20);
+            this.nameTB.TabIndex = 1;
+            // 
+            // stButton1
+            // 
+            this.stButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stButton1.Location = new System.Drawing.Point(220, 29);
+            this.stButton1.Name = "stButton1";
+            this.stButton1.Size = new System.Drawing.Size(54, 23);
+            this.stButton1.TabIndex = 2;
+            this.stButton1.Text = "Edit:";
+            this.stButton1.UseVisualStyleBackColor = false;
+            // 
+            // stLabel2
+            // 
+            this.stLabel2.AutoSize = true;
+            this.stLabel2.Location = new System.Drawing.Point(3, 61);
+            this.stLabel2.Name = "stLabel2";
+            this.stLabel2.Size = new System.Drawing.Size(48, 13);
+            this.stLabel2.TabIndex = 3;
+            this.stLabel2.Text = "Sampler:";
             // 
             // samplerCB
             // 
@@ -132,75 +139,44 @@
             this.samplerCB.Size = new System.Drawing.Size(206, 21);
             this.samplerCB.TabIndex = 4;
             // 
-            // stLabel2
+            // stTextBox2
             // 
-            this.stLabel2.AutoSize = true;
-            this.stLabel2.Location = new System.Drawing.Point(3, 61);
-            this.stLabel2.Name = "stLabel2";
-            this.stLabel2.Size = new System.Drawing.Size(48, 13);
-            this.stLabel2.TabIndex = 3;
-            this.stLabel2.Text = "Sampler:";
+            this.stTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.stTextBox2.Location = new System.Drawing.Point(67, 85);
+            this.stTextBox2.Name = "stTextBox2";
+            this.stTextBox2.Size = new System.Drawing.Size(207, 20);
+            this.stTextBox2.TabIndex = 5;
             // 
-            // stButton1
+            // stLabel3
             // 
-            this.stButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.stButton1.Location = new System.Drawing.Point(220, 29);
-            this.stButton1.Name = "stButton1";
-            this.stButton1.Size = new System.Drawing.Size(54, 23);
-            this.stButton1.TabIndex = 2;
-            this.stButton1.Text = "Edit:";
-            this.stButton1.UseVisualStyleBackColor = false;
-            // 
-            // nameTB
-            // 
-            this.nameTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.nameTB.Location = new System.Drawing.Point(8, 32);
-            this.nameTB.Name = "nameTB";
-            this.nameTB.Size = new System.Drawing.Size(207, 20);
-            this.nameTB.TabIndex = 1;
-            // 
-            // stLabel1
-            // 
-            this.stLabel1.AutoSize = true;
-            this.stLabel1.Location = new System.Drawing.Point(5, 10);
-            this.stLabel1.Name = "stLabel1";
-            this.stLabel1.Size = new System.Drawing.Size(46, 13);
-            this.stLabel1.TabIndex = 0;
-            this.stLabel1.Text = "Texture:";
-            // 
-            // stPropertyGrid1
-            // 
-            this.stPropertyGrid1.AutoScroll = true;
-            this.stPropertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.stPropertyGrid1.Location = new System.Drawing.Point(0, 0);
-            this.stPropertyGrid1.Name = "stPropertyGrid1";
-            this.stPropertyGrid1.ShowHintDisplay = true;
-            this.stPropertyGrid1.Size = new System.Drawing.Size(304, 299);
-            this.stPropertyGrid1.TabIndex = 0;
+            this.stLabel3.AutoSize = true;
+            this.stLabel3.Location = new System.Drawing.Point(5, 87);
+            this.stLabel3.Name = "stLabel3";
+            this.stLabel3.Size = new System.Drawing.Size(29, 13);
+            this.stLabel3.TabIndex = 6;
+            this.stLabel3.Text = "Hint:";
             // 
             // textureBP
             // 
+            this.textureBP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textureBP.BackColor = System.Drawing.Color.Transparent;
             this.textureBP.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("textureBP.BackgroundImage")));
-            this.textureBP.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textureBP.Location = new System.Drawing.Point(0, 0);
+            this.textureBP.Location = new System.Drawing.Point(3, 111);
             this.textureBP.Name = "textureBP";
-            this.textureBP.Size = new System.Drawing.Size(585, 219);
+            this.textureBP.Size = new System.Drawing.Size(272, 318);
             this.textureBP.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.textureBP.TabIndex = 0;
+            this.textureBP.TabIndex = 7;
             this.textureBP.TabStop = false;
             // 
             // SamplerEditorSimple
             // 
+            this.Controls.Add(this.splitContainer2);
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.splitContainer1);
             this.Name = "SamplerEditorSimple";
             this.Size = new System.Drawing.Size(585, 522);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
             this.splitContainer2.Panel1.ResumeLayout(false);
             this.splitContainer2.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
@@ -214,9 +190,9 @@
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.SplitContainer splitContainer2;
         private Switch_Toolbox.Library.Forms.STPanel stPanel1;
+        private Switch_Toolbox.Library.Forms.PictureBoxCustom textureBP;
         private Switch_Toolbox.Library.Forms.STLabel stLabel3;
         private Switch_Toolbox.Library.Forms.STTextBox stTextBox2;
         private Switch_Toolbox.Library.Forms.STComboBox samplerCB;
@@ -225,6 +201,5 @@
         private Switch_Toolbox.Library.Forms.STTextBox nameTB;
         private Switch_Toolbox.Library.Forms.STLabel stLabel1;
         private Switch_Toolbox.Library.Forms.STPropertyGrid stPropertyGrid1;
-        private Switch_Toolbox.Library.Forms.PictureBoxCustom textureBP;
     }
 }
