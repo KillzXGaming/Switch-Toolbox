d228920614541b7fb0a3fcb4bc7456462ece3f2a
KillzXGaming <millernathan35@gmail.com>
2019-06-26 15:18:48 -0400

    Throw error if 0.9 bfres are saved

--- Switch_FileFormatsMain/FileFormats/BFRES/BFRES.cs
+++ Switch_FileFormatsMain/FileFormats/BFRES/BFRES.cs
@@ -1337,6 +1337,8 @@ namespace FirstPlugin
         private void SaveSwitch(MemoryStream mem)
         {
             var resFile = BFRESRender.ResFileNode.resFile;
+            if (resFile.VersionMajor2 == 9)
+                throw new NotImplementedException($"Bfres version does not support saving yet! {resFile.VersionFull}");
 
             resFile.Models.Clear();
             resFile.SkeletalAnims.Clear();
