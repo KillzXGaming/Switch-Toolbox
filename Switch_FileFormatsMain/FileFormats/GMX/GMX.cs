0668d83a65ca8980892da93968c784694187b66a
KillzXGaming <millernathan35@gmail.com>
2019-07-04 20:03:27 -0400

    Fix vertex color format list

---
+++ Switch_FileFormatsMain/FileFormats/GMX/GMX.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Switch_Toolbox;
+using System.Windows.Forms;
+using Switch_Toolbox.Library;
+
+namespace FirstPlugin
+{
+    public class GMX : IFileFormat
+    {
+        public FileType FileType { get; set; } = FileType.Model;
+
+        public bool CanSave { get; set; }
+        public string[] Description { get; set; } = new string[] { "GMX" };
+        public string[] Extension { get; set; } = new string[] { "*.gmx" };
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public IFileInfo IFileInfo { get; set; }
+
+        public bool Identify(System.IO.Stream stream)
+        {
+            using (var reader = new Switch_Toolbox.Library.IO.FileReader(stream, true))
+            {
+                return reader.CheckSignature(4, "GMX2");
+            }
+        }
+
+        public Type[] Types
+        {
+            get
+            {
+                List<Type> types = new List<Type>();
+                return types.ToArray();
+            }
+        }
+
+        public void Load(System.IO.Stream stream)
+        {
+
+        }
+        public void Unload()
+        {
+
+        }
+        public byte[] Save()
+        {
+            return null;
+        }
+    }
+}
--- Switch_FileFormatsMain/GUI/BFRES/BfresModelImportSettings.cs
+++ Switch_FileFormatsMain/GUI/BFRES/BfresModelImportSettings.cs
@@ -327,12 +327,12 @@ namespace FirstPlugin
             comboBoxFormatUvs.Items.Add(AttribFormat.Format_8_8_UNorm);
             comboBoxFormatUvs.SelectedIndex = 1;
 
-            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_32_32_Single);
-            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_16_16_Single);
-            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_16_16_SNorm);
-            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_10_10_10_2_SNorm);
-            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_8_8_SNorm);
-            comboBoxFormatVertexColors.SelectedIndex = 3;
+            comboBoxFormatTangents.Items.Add(AttribFormat.Format_32_32_32_32_Single);
+            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_16_16_16_16_Single);
+            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_16_16_16_16_SNorm);
+            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_8_8_8_8_UNorm);
+            comboBoxFormatVertexColors.Items.Add(AttribFormat.Format_8_8_8_8_SNorm);
+            comboBoxFormatVertexColors.SelectedIndex = 1;
 
             comboBoxFormatFaces.Items.Add(IndexFormat.UInt16);
             comboBoxFormatFaces.Items.Add(IndexFormat.UInt32);
