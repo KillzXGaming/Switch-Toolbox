cf0e5ccc26bde8ac9755b81a2fd9d88ac44b96b3
KillzXGaming <millernathan35@gmail.com>
2019-12-12 20:02:27 -0500

    Fix errors

---
+++ Switch_Toolbox_Library/Interfaces/IArchiveQuickAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Library
+{
+    public interface IArchiveQuickAccess
+    {
+        /// <summary>
+        /// Determines the category. The keys are the extension, value is category.
+        /// </summary>
+        Dictionary<string, string> CategoryLookup { get; }
+    }
+}
