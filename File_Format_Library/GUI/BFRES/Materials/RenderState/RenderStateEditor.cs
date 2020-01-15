d53ee25237a472b37e7becbb11367be831c80802
KillzXGaming <millernathan35@gmail.com>
2020-01-14 20:49:12 -0500

    Fix errors

---
+++ File_Format_Library/GUI/BFRES/Materials/RenderState/RenderStateEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstPlugin.GUI.BFRES.Materials.RenderState
+{
+    public partial class RenderStateEditor : UserControl
+    {
+        public RenderStateEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
