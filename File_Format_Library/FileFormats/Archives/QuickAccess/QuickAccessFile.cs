a5d58aa26803809e67063b6f1087dd8e6da89973
KillzXGaming <millernathan35@gmail.com>
2020-01-22 19:14:20 -0500

    Fix KCL AttributeByml generation for SMO materials

---
+++ File_Format_Library/FileFormats/Archives/QuickAccess/QuickAccessFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.Library.Forms;
+using Toolbox.Library;
+using System.Windows.Forms;
+
+namespace FirstPlugin
+{
+    public class QuickAccessFile : TreeNodeCustom
+    {
+        public QuickAccessFile(string text)
+        {
+            Text = text;
+        }
+
+        public override void OnClick(TreeView treeview)
+        {
+            ArchiveFilePanel editor = (ArchiveFilePanel)LibraryGUI.GetActiveContent(typeof(ArchiveFilePanel));
+            if (editor == null)
+            {
+                editor = new ArchiveFilePanel();
+                editor.Dock = DockStyle.Fill;
+                LibraryGUI.LoadEditor(editor);
+            }
+
+            editor.LoadFile((ArchiveFileInfo)Tag);
+            editor.UpdateEditor();
+        }
+    }
+
+}
---
+++ File_Format_Library/FileFormats/Archives/QuickAccess/QuickAccessFolder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.Library.Forms;
+using Toolbox.Library;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FirstPlugin.FileFormats.Archives.QuickAccess
+{
+    public class QuickAccessFolder : TreeNodeCustom, IContextMenuNode
+    {
+        private IArchiveFile ArchiveFile;
+        public QuickAccessFolder(IArchiveFile archiveFile, string text)
+        {
+            Text = text;
+            ArchiveFile = archiveFile;
+        }
+
+        public virtual ToolStripItem[] GetContextMenuItems()
+        {
+            List<ToolStripItem> Items = new List<ToolStripItem>();
+            Items.Add(new ToolStripMenuItem("Export All", null, ExportAllAction, Keys.Control | Keys.E));
+            Items.Add(new ToolStripMenuItem("Replace (From Folder)", null, ReplaceAllAction, Keys.Control | Keys.R));
+            return Items.ToArray();
+        }
+
+        private void ExportAllAction(object sender, EventArgs args)
+        {
+            FolderSelectDialog folderDialog = new FolderSelectDialog();
+            if (folderDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            foreach (TreeNode folder in Nodes)
+            {
+                string path = Path.Combine(folderDialog.SelectedPath, Text, folder.Text);
+
+                foreach (TreeNode file in folder.Nodes)
+                {
+                    if (file.Tag is ArchiveFileInfo)
+                    {
+                        var fileInfo = (ArchiveFileInfo)file.Tag;
+                        var filePath = Path.Combine(path, file.Text);
+
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+
+                        File.WriteAllBytes(filePath, fileInfo.FileData);
+                    }
+                }
+            }
+        }
+
+        private void ReplaceAllAction(object sender, EventArgs args)
+        {
+            FolderSelectDialog folderDialog = new FolderSelectDialog();
+            if (folderDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            foreach (var folder in Directory.GetDirectories(folderDialog.SelectedPath))
+            {
+                foreach (var file in Directory.GetFiles(folder))
+                {
+                    var fileInfo = ArchiveFile.Files.FirstOrDefault(x =>
+                         Path.GetFileName(x.FileName).Contains(Path.GetFileName(file)));
+
+                    if (fileInfo != null)
+                        fileInfo.FileData = File.ReadAllBytes(file);
+                }
+            }
+        }
+    }
+
+}
