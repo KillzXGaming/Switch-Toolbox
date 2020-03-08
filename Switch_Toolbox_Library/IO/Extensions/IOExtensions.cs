81e48cd20e424a50c23fe6cf5633585cf51afe42
KillzXGaming <millernathan35@gmail.com>
2020-03-08 19:11:44 -0400

    Update framework reference so it's the older one

--- Switch_Toolbox_Library/IO/Extensions/IOExtensions.cs
+++ Switch_Toolbox_Library/IO/Extensions/IOExtensions.cs
@@ -83,20 +83,17 @@ namespace Toolbox.Library.IO
             if (!isBigEndian)
                 return;
 
-            Console.WriteLine("type " + type + " " + type.IsPrimitive);
-
             if (type.IsPrimitive)
             {
                 if (type == typeof(short) || type == typeof(ushort) ||
                  type == typeof(int) || type == typeof(uint) ||
-                 type == typeof(long) || type == typeof(ulong))
+                 type == typeof(long) || type == typeof(ulong) ||
+                  type == typeof(double) || type == typeof(float))
                 {
                     Array.Reverse(buffer);
                     return;
                 }
             }
-
-            Console.WriteLine("GetFields " + type.GetFields().Length);
 
             foreach (var field in type.GetFields())
             {
@@ -108,26 +105,27 @@ namespace Toolbox.Library.IO
                 if (fieldType.BaseType == typeof(Enum) && fieldType != typeof(ByteOrder))
                     fieldType = fieldType.GetFields()[0].FieldType;
 
-                // Swap bytes only for the following types (incomplete just like BinaryReaderX is)
+                var offset = Marshal.OffsetOf(type, field.Name).ToInt32();
+                // Enums
+                if (fieldType.IsEnum)
+                    fieldType = Enum.GetUnderlyingType(fieldType);
+
+                // Check for sub-fields to recurse if necessary
+                var subFields = fieldType.GetFields().Where(subField => subField.IsStatic == false).ToArray();
+                var effectiveOffset = startOffset + offset;
+
                 if (fieldType == typeof(short) || fieldType == typeof(ushort) ||
                     fieldType == typeof(int) || fieldType == typeof(uint) ||
-                    fieldType == typeof(long) || fieldType == typeof(ulong))
+                    fieldType == typeof(long) || fieldType == typeof(ulong) ||
+                    fieldType == typeof(double) || fieldType == typeof(float))
                 {
-                    var offset = Marshal.OffsetOf(type, field.Name).ToInt32();
-
-                    // Enums
-                    if (fieldType.IsEnum)
-                        fieldType = Enum.GetUnderlyingType(fieldType);
-
-                    // Check for sub-fields to recurse if necessary
-                    var subFields = fieldType.GetFields().Where(subField => subField.IsStatic == false).ToArray();
-                    var effectiveOffset = startOffset + offset;
-
                     if (subFields.Length == 0)
                         Array.Reverse(buffer, effectiveOffset, Marshal.SizeOf(fieldType));
-                    else
-                        AdjustBigEndianByteOrder(fieldType, buffer, isBigEndian, effectiveOffset);
+          
                 }
+
+                if (subFields.Length > 0)
+                    AdjustBigEndianByteOrder(fieldType, buffer, isBigEndian, effectiveOffset);
             }
         }
 
