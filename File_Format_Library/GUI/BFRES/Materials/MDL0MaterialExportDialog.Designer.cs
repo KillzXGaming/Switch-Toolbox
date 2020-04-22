0fd044159350c83b636d4bc8c87e607f5f3296d6
HelloOO7 <cendarehor@gmail.com>
2020-04-22 13:41:08 +0200

    Add WinForms dialogs for MDL0Mat exporting.

---
+++ File_Format_Library/GUI/BFRES/Materials/MDL0MaterialExportDialog.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace FirstPlugin.GUI.BFRES.Materials
+{
+    partial class MDL0MaterialExportDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.albCheckBox = new Toolbox.Library.Forms.STCheckBox();
+            this.emmCheckBox = new Toolbox.Library.Forms.STCheckBox();
+            this.bake0CheckBox = new Toolbox.Library.Forms.STCheckBox();
+            this.bake1CheckBox = new Toolbox.Library.Forms.STCheckBox();
+            this.btnConfirm = new Toolbox.Library.Forms.STButton();
+            this.btnCancel = new Toolbox.Library.Forms.STButton();
+            this.contentContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contentContainer
+            // 
+            this.contentContainer.Controls.Add(this.btnCancel);
+            this.contentContainer.Controls.Add(this.btnConfirm);
+            this.contentContainer.Controls.Add(this.bake1CheckBox);
+            this.contentContainer.Controls.Add(this.bake0CheckBox);
+            this.contentContainer.Controls.Add(this.emmCheckBox);
+            this.contentContainer.Controls.Add(this.albCheckBox);
+            this.contentContainer.Location = new System.Drawing.Point(0, 0);
+            this.contentContainer.Size = new System.Drawing.Size(251, 207);
+            this.contentContainer.Controls.SetChildIndex(this.albCheckBox, 0);
+            this.contentContainer.Controls.SetChildIndex(this.emmCheckBox, 0);
+            this.contentContainer.Controls.SetChildIndex(this.bake0CheckBox, 0);
+            this.contentContainer.Controls.SetChildIndex(this.bake1CheckBox, 0);
+            this.contentContainer.Controls.SetChildIndex(this.btnConfirm, 0);
+            this.contentContainer.Controls.SetChildIndex(this.btnCancel, 0);
+            // 
+            // albCheckBox
+            // 
+            this.albCheckBox.AutoSize = true;
+            this.albCheckBox.Checked = true;
+            this.albCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.albCheckBox.Location = new System.Drawing.Point(39, 49);
+            this.albCheckBox.Name = "albCheckBox";
+            this.albCheckBox.Size = new System.Drawing.Size(125, 17);
+            this.albCheckBox.TabIndex = 0;
+            this.albCheckBox.Text = "Include Diffuse maps";
+            this.albCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // emmCheckBox
+            // 
+            this.emmCheckBox.AutoSize = true;
+            this.emmCheckBox.Checked = true;
+            this.emmCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.emmCheckBox.Location = new System.Drawing.Point(39, 73);
+            this.emmCheckBox.Name = "emmCheckBox";
+            this.emmCheckBox.Size = new System.Drawing.Size(133, 17);
+            this.emmCheckBox.TabIndex = 1;
+            this.emmCheckBox.Text = "Include Emission maps";
+            this.emmCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // bake0CheckBox
+            // 
+            this.bake0CheckBox.AutoSize = true;
+            this.bake0CheckBox.Checked = true;
+            this.bake0CheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bake0CheckBox.Location = new System.Drawing.Point(39, 97);
+            this.bake0CheckBox.Name = "bake0CheckBox";
+            this.bake0CheckBox.Size = new System.Drawing.Size(155, 17);
+            this.bake0CheckBox.TabIndex = 2;
+            this.bake0CheckBox.Text = "Include Shadow bakemaps";
+            this.bake0CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // bake1CheckBox
+            // 
+            this.bake1CheckBox.AutoSize = true;
+            this.bake1CheckBox.Checked = true;
+            this.bake1CheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bake1CheckBox.Location = new System.Drawing.Point(39, 121);
+            this.bake1CheckBox.Name = "bake1CheckBox";
+            this.bake1CheckBox.Size = new System.Drawing.Size(139, 17);
+            this.bake1CheckBox.TabIndex = 3;
+            this.bake1CheckBox.Text = "Include Light bakemaps";
+            this.bake1CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirm.Location = new System.Drawing.Point(39, 149);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.TabIndex = 4;
+            this.btnConfirm.Text = "Export";
+            this.btnConfirm.UseVisualStyleBackColor = false;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Location = new System.Drawing.Point(137, 149);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // MDL0MaterialExportDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 206);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MDL0MaterialExportDialog";
+            this.Text = "Export options";
+            this.contentContainer.ResumeLayout(false);
+            this.contentContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Toolbox.Library.Forms.STCheckBox albCheckBox;
+        private Toolbox.Library.Forms.STCheckBox emmCheckBox;
+        private Toolbox.Library.Forms.STCheckBox bake0CheckBox;
+        private Toolbox.Library.Forms.STCheckBox bake1CheckBox;
+        private Toolbox.Library.Forms.STButton btnConfirm;
+        private Toolbox.Library.Forms.STButton btnCancel;
+    }
+}
---
+++ File_Format_Library/GUI/BFRES/Materials/MDL0MaterialExportDialog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Toolbox.Library.Forms;
+
+namespace FirstPlugin.GUI.BFRES.Materials
+{
+    public partial class MDL0MaterialExportDialog : STForm
+    {
+        String ExportFlags = "UNSET";
+
+        public MDL0MaterialExportDialog()
+        {
+            InitializeComponent();
+        }
+
+        public String GetExportFlags()
+        {            
+            return ExportFlags;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ExportFlags = "UNSET";
+            this.Close();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            String output = "";
+            if (albCheckBox.Checked)
+            {
+                output += "A";
+            }
+            if (emmCheckBox.Checked)
+            {
+                output += "E";
+            }
+            if (bake0CheckBox.Checked)
+            {
+                output += "S";
+            }
+            if (bake1CheckBox.Checked)
+            {
+                output += "L";
+            }
+            ExportFlags = output;
+            this.Close();
+        }
+    }
+}
