549f23153f83261a11f9a64ec181aae98e009e48
KillzXGaming <millernathan35@gmail.com>
2020-08-31 20:18:07 -0400

    Try patching from appveyor settings on the site

--- Toolbox/MainForm.cs
+++ Toolbox/MainForm.cs
@@ -614,10 +614,8 @@ namespace Toolbox
         {
             string commit = $"Commit: {Runtime.CommitInfo}";
             var asssemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
-            var version = $"{asssemblyVersion.Major}.{asssemblyVersion.MajorRevision}.{asssemblyVersion.MinorRevision}";
-
             if (DisplayVersion)
-                Text = $"{Application.ProductName} | Version: {Runtime.ProgramVersion} | {commit} | Compile Date: {Runtime.CompileDate} Assembly {version}";
+                Text = $"{Application.ProductName} | Version: {Runtime.ProgramVersion} | {commit} | Compile Date: {Runtime.CompileDate} Assembly {asssemblyVersion}";
             else
                 Text = $"{Application.ProductName}";
         }
