ae515217b5d12aceb9b12c8b1dc39c3e38c39e12
KillzXGaming <millernathan35@gmail.com>
2021-06-15 18:20:01 -0400

    Remove some unused old code in seanim

--- Switch_Toolbox_Library/FileFormats/Animation/SEANIM.cs
+++ Switch_Toolbox_Library/FileFormats/Animation/SEANIM.cs
@@ -209,32 +209,6 @@ namespace Toolbox.Library.Animations
                 anim.NextFrame(skeleton);
             anim.NextFrame(skeleton);  //go on first frame
 
-            foreach (STBone b in skeleton.getBoneTreeOrder())
-            {
-                if (anim.HasBone(b.Text))
-                {
-                    Animation.KeyNode n = anim.GetBone(b.Text);
-                    if (n.XPOS.HasAnimation())
-                        WriteKey(n.XPOS);
-                    if (n.YPOS.HasAnimation())
-                        WriteKey(n.YPOS);
-                    if (n.ZPOS.HasAnimation())
-                        WriteKey(n.ZPOS);
-                    if (n.XROT.HasAnimation())
-                        WriteKey(n.XROT);
-                    if (n.YROT.HasAnimation())
-                        WriteKey(n.YROT);
-                    if (n.ZROT.HasAnimation())
-                        WriteKey(n.ZROT);
-                    if (n.XSCA.HasAnimation())
-                        WriteKey(n.XSCA);
-                    if (n.YSCA.HasAnimation())
-                        WriteKey(n.YSCA);
-                    if (n.ZSCA.HasAnimation())
-                        WriteKey(n.ZSCA);
-                }
-            }
-
             SEAnim seAnim = new SEAnim();
             seAnim.Looping = anim.CanLoop;
             seAnim.AnimType = AnimationType.Absolute;
