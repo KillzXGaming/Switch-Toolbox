074629e4d399c49aa6635a7b7097dca4f817b578
edbefee3-3888-462a-9411-741b7e9eb54e <tamoozbr@gmail.com>
2022-01-14 06:32:42 +0200

    Reduced amount of times the string "mario" is checked for each mesh

--- File_Format_Library/GUI/SMO/MarioCostumeEditor.cs
+++ File_Format_Library/GUI/SMO/MarioCostumeEditor.cs
@@ -15,102 +15,105 @@ namespace FirstPlugin
             Vector3 scale;
             Vector3 rotation;
 
-            if (fileName.Contains("Mario") && fileName.Contains("Face"))
+            if (fileName.Contains("Mario"))
             {
-                Console.WriteLine("Positioning Face Mesh.....");
-                position = new Vector3(0, 97.0f, 0);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("Head"))
-            {
-                Console.WriteLine("Positioning Head Mesh.....");
-                position = new Vector3(0, 97.0f, 0);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("HandL"))
-            {
-                Console.WriteLine("Positioning Face Mesh.....");
-                position = new Vector3(48.877f, 82.551f, -3.3f);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 90f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("HandR"))
-            {
-                Console.WriteLine("Positioning HandR Mesh.....");
-                position = new Vector3(-48.877f, 82.551f, -3.3f);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, -90f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("Eye"))
-            {
-                Console.WriteLine("Positioning Eye Mesh.....");
-                position = new Vector3(0, 97.0f, 0);
-                scale = new Vector3(1, 1, 1);
+                if (fileName.Contains("Face"))
+                    {
+                    Console.WriteLine("Positioning Face Mesh.....");
+                    position = new Vector3(0, 97.0f, 0);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0, 0);
+                }
+                if (fileName.Contains("Head"))
+                {
+                    Console.WriteLine("Positioning Head Mesh.....");
+                    position = new Vector3(0, 97.0f, 0);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0, 0);
+                }
+                if (fileName.Contains("HandL"))
+                {
+                    Console.WriteLine("Positioning HandR Mesh.....");
+                    position = new Vector3(48.877f, 82.551f, -3.3f);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 90f, 0);
+                }
+                if (fileName.Contains("HandR"))
+                {
+                    Console.WriteLine("Positioning HandR Mesh.....");
+                    position = new Vector3(-48.877f, 82.551f, -3.3f);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, -90f, 0);
+                }
+                if (fileName.Contains("Eye"))
+                {
+                    Console.WriteLine("Positioning Eye Mesh.....");
+                    position = new Vector3(0, 97.0f, 0);
+                    scale = new Vector3(1, 1, 1);
                 rotation = new Vector3(0, 0f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("Hair"))
-            {
-                Console.WriteLine("Positioning Hair Mesh.....");
-                position = new Vector3(0, 97.0f, 0);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("Skirt"))
-            {
-                Console.WriteLine("Positioning Skirt Mesh.....");
-                position = new Vector3(0, 56.0f, 0);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("Tail"))
-            {
-                Console.WriteLine("Positioning Tail Mesh.....");
-                position = new Vector3(0, 56.0f, 0);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("Shell"))
-            {
-                Console.WriteLine("Positioning Tail Mesh.....");
-                position = new Vector3(0, 75.0f, 0);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("aHakama"))
-            {
-                Console.WriteLine("Positioning Hakama Mesh.....");
-                position = new Vector3(0, 61.0f, -3.0f);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("Under"))
-            {
-                Console.WriteLine("Positioning Under Mesh.....");
-                position = new Vector3(0, 56.0f, 0);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("PonchoPoncho"))
-            {
-                Console.WriteLine("Positioning Poncho Mesh.....");
-                position = new Vector3(0, 60.5f, -4.0f);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0f, 0);
-            }
-            else if (fileName.Contains("Mario") && fileName.Contains("PonchoGuitar"))
-            {
-                Console.WriteLine("Positioning Guitar Mesh.....");
-                position = new Vector3(48.877f, 0, -12.3f);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 90f, 0);
-            }
-            else
-            {
-                position = new Vector3(0, 0, 0);
-                scale = new Vector3(1, 1, 1);
-                rotation = new Vector3(0, 0, 0);
+                }
+                if (fileName.Contains("Hair"))
+                {
+                    Console.WriteLine("Positioning Hair Mesh.....");
+                    position = new Vector3(0, 97.0f, 0);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0f, 0);
+                }
+                if (fileName.Contains("Skirt"))
+                {
+                    Console.WriteLine("Positioning Skirt Mesh.....");
+                    position = new Vector3(0, 56.0f, 0);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0f, 0);
+                }
+                if (fileName.Contains("Tail"))
+                {
+                    Console.WriteLine("Positioning Tail Mesh.....");
+                    position = new Vector3(0, 56.0f, 0);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0f, 0);
+                }
+                if (fileName.Contains("Shell"))
+                {
+                    Console.WriteLine("Positioning Shell Mesh.....");
+                    position = new Vector3(0, 75.0f, 0);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0f, 0);
+                }
+                if (fileName.Contains("aHakama"))
+                {
+                    Console.WriteLine("Positioning Hakama Mesh.....");
+                    position = new Vector3(0, 61.0f, -3.0f);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0f, 0);
+                }
+                if (fileName.Contains("Under"))
+                {
+                    Console.WriteLine("Positioning Under Mesh.....");
+                    position = new Vector3(0, 56.0f, 0);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0f, 0);
+                }
+                if (fileName.Contains("PonchoPoncho"))
+                {
+                    Console.WriteLine("Positioning Poncho Mesh.....");
+                    position = new Vector3(0, 60.5f, -4.0f);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0f, 0);
+                }
+                if (fileName.Contains("PonchoGuitar"))
+                {
+                    Console.WriteLine("Positioning Guitar Mesh.....");
+                    position = new Vector3(48.877f, 0, -12.3f);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 90f, 0);
+                }
+                else
+                {
+                    position = new Vector3(0, 0, 0);
+                    scale = new Vector3(1, 1, 1);
+                    rotation = new Vector3(0, 0, 0);
+                }
             }
 
             Matrix4 positionMat = Matrix4.CreateTranslation(position);
