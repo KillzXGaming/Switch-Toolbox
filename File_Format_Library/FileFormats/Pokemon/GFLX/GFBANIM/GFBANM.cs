7ba0eb7dcc168178efb8d909aac27bf93f96d4d6
KillzXGaming <millernathan35@gmail.com>
2022-05-25 16:41:51 -0400

    GFBANIM : Scale like maya. Todo: no idea what toggles this.

--- File_Format_Library/FileFormats/Pokemon/GFLX/GFBANIM/GFBANM.cs
+++ File_Format_Library/FileFormats/Pokemon/GFLX/GFBANIM/GFBANM.cs
@@ -191,6 +191,8 @@ namespace FirstPlugin
                         b = skeleton.GetBone(node.Name);
                         if (b == null) continue;
 
+                        b.UseSegmentScaleCompensate = true;
+
                         Updated = true;
 
                         if (node.TranslateX.HasKeys)
