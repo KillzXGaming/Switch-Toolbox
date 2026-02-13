6875695fa860ae2be830f15c630d1f68a050a3d4
blitzingeagle <mcmorrischen@gmail.com>
2026-02-13 10:17:04 +0900

    Implement glTF export with normals and UV0 with basic materials

--- Switch_Toolbox_Library/FileFormats/GLTF/GLTF.cs
+++ Switch_Toolbox_Library/FileFormats/GLTF/GLTF.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Toolbox.Library.GLTFModel;
+using static Toolbox.Library.STGenericObject;
+using Toolbox.Library.IO;
 
 namespace Toolbox.Library
 {
@@ -11,7 +17,9 @@ namespace Toolbox.Library
     {
         public class ExportSettings
         {
-
+            public bool UseVertexColors = true;
+            public bool FlipTexCoordsVertical = false;
+            public bool UseTextureChannelComponents = true;
         }
 
         public static void Export(string FileName, ExportSettings settings, STGenericModel model, List<STGenericTexture> Textures, STSkeleton skeleton = null, List<int> NodeArray = null)
@@ -23,9 +31,116 @@ namespace Toolbox.Library
             List<STGenericObject> Meshes, List<STGenericMaterial> Materials,
             List<STGenericTexture> Textures, STSkeleton skeleton = null, List<int> NodeArray = null)
         {
-            Console.WriteLine("Hello world from GLTF.cs");
-
             var Exporter = new GLTFExporter();
+            
+            for (int i = 0; i < Textures?.Count; i++)
+            {
+                try
+                {
+                    var bitmap = Textures[i].GetBitmap();
+                    if (bitmap != null)
+                    {
+                        if (settings.UseTextureChannelComponents)
+                            bitmap = Textures[i].GetComponentBitmap(bitmap);
+                        string textureName = Textures[i].Text;
+                            //.RemoveIllegaleFileNameCharacters();
+
+                        Exporter.AddTextureMap(textureName, bitmap);
+                    }
+
+                    bitmap.Dispose();
+
+                    GC.Collect();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            foreach (var mat in Materials)
+            {
+                Exporter.AddMaterial(mat);
+            }
+            
+            foreach (var mesh in Meshes)
+            {
+                List<Vector3> Vertices = new List<Vector3>();
+                List<Vector3> Normals = new List<Vector3>();
+                List<Vector2> UV0 = new List<Vector2>();
+                List<int> TriangleFaces = new List<int>();
+
+                bool HasNormals = false;
+                bool HasColors = false;
+                bool HasColors2 = false;
+                bool HasColors3 = false;
+                bool HasColors4 = false;
+                bool HasUV0 = false;
+                bool HasUV1 = false;
+                bool HasUV2 = false;
+                bool HasUV3 = false;
+                bool HasBoneIds = false;
+
+                foreach (var vertex in mesh.vertices)
+                {
+                    if (vertex.nrm != OpenTK.Vector3.Zero) HasNormals = true;
+                    if (settings.UseVertexColors) HasColors = true;
+                    if (vertex.col2 != OpenTK.Vector4.One && settings.UseVertexColors) HasColors2 = true;
+                    if (vertex.col3 != OpenTK.Vector4.One && settings.UseVertexColors) HasColors3 = true;
+                    if (vertex.col4 != OpenTK.Vector4.One && settings.UseVertexColors) HasColors4 = true;
+
+                    if (vertex.uv0 != OpenTK.Vector2.Zero) HasUV0 = true;
+                    if (vertex.uv1 != OpenTK.Vector2.Zero) HasUV1 = true;
+                    if (vertex.uv2 != OpenTK.Vector2.Zero) HasUV2 = true;
+                    if (vertex.uv3 != OpenTK.Vector2.Zero) HasUV3 = true;
+                    if (vertex.boneIds.Count > 0) HasBoneIds = true;
+
+                    Vertices.Add(new Vector3(vertex.pos.X, vertex.pos.Y, vertex.pos.Z));
+                    Normals.Add(new Vector3(vertex.nrm.X, vertex.nrm.Y, vertex.nrm.Z));
+
+                    if (settings.FlipTexCoordsVertical)
+                    {
+                        UV0.Add(new Vector2(vertex.uv0.X, 1 - vertex.uv0.Y));
+                    }
+                    else
+                    {
+                        UV0.Add(new Vector2(vertex.uv0.X, vertex.uv0.Y));
+                    }
+                }
+
+                if (mesh.lodMeshes.Count > 0)
+                {
+                    var lodMesh = mesh.lodMeshes[mesh.DisplayLODIndex];
+
+                    List<int> faces = new List<int>();
+                    if (lodMesh.PrimativeType == STPrimitiveType.TrangleStrips)
+                        faces = STGenericObject.ConvertTriangleStripsToTriangles(lodMesh.faces);
+                    else
+                        faces = lodMesh.faces;
+
+                    if (faces.Count % 3 != 0)
+                        throw new Exception("Expected a multiple of 3!");
+
+                    TriangleFaces.AddRange(faces);
+                }
+                foreach (var group in mesh.PolygonGroups)
+                {
+                    List<int> faces = new List<int>();
+                    if (group.PrimativeType == STPrimitiveType.TrangleStrips)
+                        faces = STGenericObject.ConvertTriangleStripsToTriangles(group.faces);
+                    else
+                        faces = group.faces;
+
+                    if (faces.Count % 3 != 0)
+                        throw new Exception("Expected a multiple of 3!");
+
+                    TriangleFaces.AddRange(faces);
+                }
+
+                //Exporter.AddMeshNormal(Vertices, Normals, TriangleFaces, mesh.MaterialIndex);
+                Exporter.AddMeshNormalUV(Vertices, Normals, UV0, TriangleFaces, mesh.MaterialIndex);
+            }
+
             Exporter.Write(FileName);
         }
     }
--- Switch_Toolbox_Library/FileFormats/GLTF/GLTFExporter.cs
+++ Switch_Toolbox_Library/FileFormats/GLTF/GLTFExporter.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using SharpGLTF.Geometry;
+using SharpGLTF.Geometry.VertexTypes;
 using SharpGLTF.Materials;
 using SharpGLTF.Scenes;
 
@@ -12,11 +18,129 @@ namespace Toolbox.Library.GLTFModel
     {
         private SceneBuilder Scene;
         private MaterialBuilder DefaultMaterial;
+        private Dictionary<string, ImageBuilder> TextureMaps;
+        private List<MaterialBuilder> Materials;
 
         public GLTFExporter()
         {
             Scene = new SceneBuilder();
-            DefaultMaterial = new MaterialBuilder();
+            DefaultMaterial = new MaterialBuilder("Default");
+            TextureMaps = new Dictionary<string, ImageBuilder>();
+            Materials = new List<MaterialBuilder>();
+        }
+
+        private MaterialBuilder GetMaterial(int MaterialIndex)
+        {
+            if (MaterialIndex >= 0 && MaterialIndex < Materials.Count)
+                return Materials[MaterialIndex];
+            return DefaultMaterial;
+        }
+
+        public void AddTextureMap(string textureName, Bitmap bitmap)
+        {
+            var ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Png);
+            byte[] bytes = ms.ToArray();
+            ImageBuilder image = ImageBuilder.From(bytes);
+            image.AlternateWriteFileName = textureName;
+            TextureMaps.Add(textureName, image);
+        }
+
+        public void AddMaterial(STGenericMaterial mat)
+        {
+            string materialName = mat.Text;
+
+            MaterialBuilder material = new MaterialBuilder(materialName)
+                .WithMetallicRoughnessShader();
+
+            foreach (var tex in mat.TextureMaps)
+            {
+                ImageBuilder image = TextureMaps[tex.Name];
+
+                switch (tex.Type)
+                {
+                    case STGenericMatTexture.TextureType.Diffuse:
+                        material = material.WithChannelImage(KnownChannel.BaseColor, image);
+                        break;
+                    case STGenericMatTexture.TextureType.Normal:
+                        material = material.WithChannelImage(KnownChannel.Normal, image);
+                        break;
+                }
+            }    
+
+            Materials.Add(material);
+        }
+
+        public void AddMesh(List<Vector3> Vertices, List<int> TriangleFaces, int MaterialIndex)
+        {
+            var mesh = new MeshBuilder<VertexPosition>();
+            var prim = mesh.UsePrimitive(GetMaterial(MaterialIndex));
+
+            if (TriangleFaces.Count % 3 != 0)
+                throw new Exception("Expected a multiple of 3!");
+
+            for (int i = 0; i < TriangleFaces.Count; i += 3)
+            {
+                var v0 = new VertexPosition(Vertices[TriangleFaces[i]]);
+                var v1 = new VertexPosition(Vertices[TriangleFaces[i + 1]]);
+                var v2 = new VertexPosition(Vertices[TriangleFaces[i + 2]]);
+
+                prim.AddTriangle(v0, v1, v2);
+            }
+
+            Scene.AddRigidMesh(mesh, Matrix4x4.Identity);
+        }
+
+        public void AddMeshNormal(List<Vector3> Vertices, List<Vector3> Normals, List<int> TriangleFaces, int MaterialIndex)
+        {
+            var mesh = new MeshBuilder<VertexPositionNormal>();
+            var prim = mesh.UsePrimitive(GetMaterial(MaterialIndex));
+
+            if (TriangleFaces.Count % 3 != 0)
+                throw new Exception("Expected a multiple of 3!");
+
+            for (int i = 0; i < TriangleFaces.Count; i += 3)
+            {
+                var v0 = new VertexPositionNormal(
+                    Vertices[TriangleFaces[i]],
+                    Normals[TriangleFaces[i]]);
+                var v1 = new VertexPositionNormal(
+                    Vertices[TriangleFaces[i + 1]],
+                    Normals[TriangleFaces[i + 1]]);
+                var v2 = new VertexPositionNormal(
+                    Vertices[TriangleFaces[i + 2]],
+                    Normals[TriangleFaces[i + 2]]);
+
+                prim.AddTriangle(v0, v1, v2);
+            }
+
+            Scene.AddRigidMesh(mesh, Matrix4x4.Identity);
+        }
+
+        public void AddMeshNormalUV(List<Vector3> Vertices, List<Vector3> Normals, List<Vector2> UV0, List<int> TriangleFaces, int MaterialIndex)
+        {
+            var mesh = new MeshBuilder<VertexPositionNormal, VertexTexture1, VertexEmpty>();
+            var prim = mesh.UsePrimitive(GetMaterial(MaterialIndex));
+
+            if (TriangleFaces.Count % 3 != 0)
+                throw new Exception("Expected a multiple of 3!");
+
+            for (int i = 0; i < TriangleFaces.Count; i += 3)
+            {
+                var v0 = new VertexBuilder<VertexPositionNormal, VertexTexture1, VertexEmpty>();
+                v0.Geometry = new VertexPositionNormal(Vertices[TriangleFaces[i]], Normals[TriangleFaces[i]]);
+                v0.Material = new VertexTexture1(UV0[TriangleFaces[i]]);
+                var v1 = new VertexBuilder<VertexPositionNormal, VertexTexture1, VertexEmpty>();
+                v1.Geometry = new VertexPositionNormal(Vertices[TriangleFaces[i+1]], Normals[TriangleFaces[i+1]]);
+                v1.Material = new VertexTexture1(UV0[TriangleFaces[i+1]]);
+                var v2 = new VertexBuilder<VertexPositionNormal, VertexTexture1, VertexEmpty>();
+                v2.Geometry = new VertexPositionNormal(Vertices[TriangleFaces[i+2]], Normals[TriangleFaces[i+2]]);
+                v2.Material = new VertexTexture1(UV0[TriangleFaces[i+2]]);
+
+                prim.AddTriangle(v0, v1, v2);
+            }
+
+            Scene.AddRigidMesh(mesh, Matrix4x4.Identity);
         }
 
         public void Write(string FileName)
