73a8b5eef6ec9c2b503987238e52d529f5f2d283
Scutlet <103026559+Scutlet@users.noreply.github.com>
2026-05-04 23:30:16 +0200

    Cafe bflyt set Windowkind
    Fixed Cafe's BFLYT WindowKind not actually being saved when changed in the editor.

--- File_Format_Library/FileFormats/Layout/CAFE/Panes/WND1.cs
+++ File_Format_Library/FileFormats/Layout/CAFE/Panes/WND1.cs
@@ -130,7 +130,14 @@ namespace LayoutBXLYT.Cafe
             set { }
         }
 
-        public WindowKind WindowKind { get; set; }
+        public WindowKind WindowKind
+        {
+            get { return (WindowKind)((_flag >> 2) & 3); }
+            set {
+                _flag = (byte)(_flag & ~(3 << 2));
+                _flag |= (byte)(((byte)value & 3) << 2);
+            }
+        }
 
         public bool NotDrawnContent
         {
@@ -243,8 +250,6 @@ namespace LayoutBXLYT.Cafe
             reader.ReadUInt16();//padding
             uint contentOffset = reader.ReadUInt32();
             uint frameOffsetTbl = reader.ReadUInt32();
-
-            WindowKind = (WindowKind)((_flag >> 2) & 3);
 
             reader.SeekBegin(pos + contentOffset);
             Content = new BxlytWindowContent(reader, header);
